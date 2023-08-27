d5f0e08666ac3ce2c526f1ec13a880c368b18d52
cmrohityadav <cmrohityadav23@gmail.com>
2023-08-27 12:10:49 +0530

    styler APP

---
+++ styler02/App.tsx
@@ -0,0 +1,19 @@
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+import FlatCard from './components/FlatCard'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+
+      <ScrollView>
+        
+        <FlatCard/>
+
+        
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default App
