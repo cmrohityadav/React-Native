da785f9ec004ed2221184cdd1dee2c9d9f4a242c
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-29 11:30:28 +0530

    yup

--- 03_/passwordgenerator/App.tsx
+++ 03_/passwordgenerator/App.tsx
@@ -1,14 +1,23 @@
 import { Text, StyleSheet, View } from 'react-native'
 import React, { Component } from 'react'
 
-export default class App extends Component {
-  render() {
+//form validation
+import * as Yup from 'yup'
+const passwordSchema=Yup.object().shape({
+  passwordLength: Yup.number()
+  .min(4,"Should be min of 4 Characters")
+  .max(16,"Should be max of 16 Characters")
+  .required("length is required")
+
+})
+export default  function App() {
+  
     return (
       <View>
         <Text>App</Text>
       </View>
     )
   }
-}
+
 
 const styles = StyleSheet.create({})
