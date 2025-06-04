e1feb2a5d19bd99ab0e6f2acc5353d613d865605
cmrohityadav <cmrohityadav23@gmail.com>
2025-06-05 00:50:33 +0530

    added flatList

--- 06_/flatlist/src/screens/FlatListScreen.tsx
+++ 06_/flatlist/src/screens/FlatListScreen.tsx
@@ -1,13 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-const FlatListScreen:React.FC = () => {
+
+// sample data for demo
+const FLAT_DATA = Array.from({length: 50}, (_, index) => ({
+  id: index.toString(),
+  title: `Item ${index + 1}`,
+}));
+const FlatListScreen: React.FC = () => {
+  const handleFlatListRenderItem = ({ item }: { item: { id: string; title: string } }) => (
+  <View style={styles.item}>
+    <Text>{item.title}</Text>
+  </View>
+);
   return (
-    <View>
-      <Text>Flat List Screen Demo</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Flat List Example</Text>
+      <FlatList
+      data={FLAT_DATA}
+      renderItem={handleFlatListRenderItem}
+      keyExtractor={(item)=>item.id}
+      ListHeaderComponent={<Text style={styles.flatListHeader}>ListHeaderComponent</Text>}
+      ListFooterComponent={<Text style={styles.flatListHeader}>ListFooterComponent</Text>}
+
+      />
     </View>
   );
 };
 
 export default FlatListScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 1,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  item:{
+    padding:20,
+    borderBottomWidth:1,
+    borderBottomColor:'#eee',
+  },
+  flatListHeader:{
+    fontSize:30,
+    fontWeight:'bold',
+  },
+});
