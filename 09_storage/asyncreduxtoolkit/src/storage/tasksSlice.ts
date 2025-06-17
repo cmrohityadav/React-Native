0cd914cb903b067055876982ec120d5afa38f0da
cmrohityadav <cmrohityadav23@gmail.com>
2025-06-17 23:10:19 +0530

    added interface for tasks

--- 09_storage/asyncreduxtoolkit/src/storage/tasksSlice.ts
+++ 09_storage/asyncreduxtoolkit/src/storage/tasksSlice.ts
@@ -1,5 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+export interface Task {
+    id :string;
+    title:string;
+    completed:string;
+}
 
+interface TasksState{
+    tasks:Task[];
+    status:'idle' | 'loading' | 'succeeded' | 'failed';
+    error:string | null;
+}
+
+const initialState: TasksState={
+    tasks:[],
+    status:'idle',
+    error: null,
+}
 const tasksSlice = createSlice({
     name:'tasks',
     initialState:{},
