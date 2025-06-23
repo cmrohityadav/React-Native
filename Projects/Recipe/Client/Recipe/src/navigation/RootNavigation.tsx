6be22986c4921da7e037ef51feecff0777f81f06
cmrohityadav <cmrohityadav23@gmail.com>
2025-06-23 23:33:16 +0530

    added basic folder structure

---
+++ Projects/Recipe/Client/Recipe/src/navigation/RootNavigation.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+export type RootStackParamsList={
+    Login:undefined;
+    
+}
+
+const RootNavigation:React.FC = () => {
+    const Stack=createStackNavigator()
+  return (
+    
+  )
+}
+
+export default RootNavigation
---
+++ Projects/Recipe/Client/Recipe/src/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const LoginScreen:React.FC= () => {
+  return (
+    <View>
+      <Text>LoginS creen</Text>
+    </View>
+  )
+}
+
+export default LoginScreen
