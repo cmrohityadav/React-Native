6969bb890d777ec890fdd514212eabb556528664
cmrohityadav <cmrohityadav23@gmail.com>
2025-06-25 21:44:28 +0530

    added style and navigation for signup screen

--- Projects/Recipe/Client/Recipe/src/screens/SignUpScreen.tsx
+++ Projects/Recipe/Client/Recipe/src/screens/SignUpScreen.tsx
@@ -1,12 +1,77 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
-
+import { StackNavigationProp } from '@react-navigation/stack'
+import { RootStackParamsList } from '../navigation/RootNavigation'
+import { useNavigation } from '@react-navigation/native'
+type SignUpScreenNavigationParams = StackNavigationProp<RootStackParamsList,'SignUp'>
 const SignUpScreen:React.FC = () => {
+    const navigation=useNavigation<SignUpScreenNavigationParams>()
   return (
-    <View>
-      <Text>Sign Up Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Sign Up </Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Email'
+        keyboardType='email-address'
+        
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Password'
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={()=> navigation.navigate('Login')}
+      >
+        <Text style={styles.linkText}>Already have an account?? Log In</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    padding:20
+  },
+  headerText:{
+    fontSize:24,
+    fontWeight:'bold',
+    marginBottom:20,
+  },
+  input:{
+    width:'100%',
+    height:45,
+    borderWidth:1,
+    borderColor:'#cccccc',
+    borderRadius:6,
+    paddingHorizontal:10,
+    marginBottom:10,
 
+  },
+  button:{
+    width:'100%',
+    height:40,
+    backgroundColor:'#007aff',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:5,
+    marginTop:10,
+  },
+  buttonText:{
+    color:'#fff',
+    fontSize:16,
+    fontWeight:'bold',
+
+  },
+  linkText:{
+    marginTop:16,
+    color:'#007aff',
+
+  }
+})
 export default SignUpScreen
