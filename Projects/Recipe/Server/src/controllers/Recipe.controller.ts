243e1c4e935770f9c3afa2d861e27f6b6fd14216
cmrohityadav <cmrohityadav23@gmail.com>
2025-06-30 23:34:01 +0530

    added getrecipe api

--- Projects/Recipe/Server/src/controllers/Recipe.controller.ts
+++ Projects/Recipe/Server/src/controllers/Recipe.controller.ts
@@ -3,16 +3,43 @@ import { AuthRequest } from "../middleware/middleware";
 import { Recipe } from "../models/Recipe.modal";
 
  export const createRecipe=async(req:AuthRequest,res:Response)=>{
-    const {title,description,difficulty}=req.body
-
-    const newRecipe= new Recipe({
-        title,description,difficulty,createdBy:req.userId
-    });
-    await newRecipe.save();
-
-    res.status(201).json({
-        success:true,
-        message:'Recipe is added successfully !',
-        data:newRecipe,
-    })
+   try {
+     const {title,description,difficulty}=req.body
+ 
+     const newRecipe= new Recipe({
+         title,description,difficulty,createdBy:req.userId
+     });
+     await newRecipe.save();
+ 
+     res.status(201).json({
+         success:true,
+         message:'Recipe is added successfully !',
+         data:newRecipe,
+     })
+     return;
+   } catch (error) {
+    console.error(error)
+    res.status(500).json({
+         success:false,
+         message:"Something went wrong ! Please try again!"
+     })
+   }
  }
+
+export const getRecipes =async(req:AuthRequest,res:Response)=>{
+    try {
+       const recipes= await Recipe.find({});
+       res.status(200).json({
+        success:true,
+        data:recipes
+       })
+
+
+    } catch (error) {
+        console.error(error)
+    res.status(500).json({
+         success:false,
+         message:"Something went wrong ! Please try again!"
+     })
+    }
+}
--- Projects/Recipe/Server/src/routes/Recipe.route.ts
+++ Projects/Recipe/Server/src/routes/Recipe.route.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import { authMiddleware } from '../middleware/middleware';
-import { createRecipe } from '../controllers/Recipe.Controller';
+import { createRecipe, getRecipes } from '../controllers/Recipe.Controller';
 
 const recipeRouter=express.Router();
 recipeRouter.route('/createRecipe').post(authMiddleware,createRecipe);
+recipeRouter.route('/getRecipes').get(authMiddleware,getRecipes);
 
 export {recipeRouter}
